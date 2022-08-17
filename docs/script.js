bf6be5f1935c6bfe6d33c168caf9d8e43dcf168d
alicaptain <aligulerworks@gmail.com>
2022-08-17 14:18:12 +0300

    Just Continue With Improvements

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,10 @@
-"use strict";
-
+'use strict';
 
 // color variabless
 
 // main front and background colors
 const blackColor = 'rgb(33,33,33)';
-const whiteColor = 'rgb(244,244,244)';
+const whiteColor = 'rgb(211,211,211)';
 
 // base shade values for rgba appliances
 const shadeBlack = '11,11,11';
@@ -13,72 +12,68 @@ const shadeWhite = '244,244,244';
 
 // set the current shader
 
-
-
-
 const docBody = document.querySelector('html');
 
 let colorNightMode = () => {
-   //assigns main front and background color
-   document.querySelector(':root').style.setProperty('--back-color', blackColor);
-   document.querySelector(':root').style.setProperty('--color', whiteColor);
+  //assigns main front and background color
+  document.querySelector(':root').style.setProperty('--back-color', blackColor);
+  document.querySelector(':root').style.setProperty('--color', whiteColor);
 
-   //shades value for rgba appliances
-   document.querySelector(':root').style.setProperty('--shade', shadeBlack);
+  //shades value for rgba appliances
+  document.querySelector(':root').style.setProperty('--shade', shadeBlack);
 
-   return;
+  return;
 };
 
 let colorDayMode = () => {
-   document.querySelector(':root').style.setProperty('--back-color', whiteColor);
-   document.querySelector(':root').style.setProperty('--color', blackColor);
+  document.querySelector(':root').style.setProperty('--back-color', whiteColor);
+  document.querySelector(':root').style.setProperty('--color', blackColor);
 
-   document.querySelector(':root').style.setProperty('--shade', shadeWhite);
+  document.querySelector(':root').style.setProperty('--shade', shadeWhite);
 
-   return;
+  return;
 };
 
-
-// Theme switcher -  according to the hour
+//SWITCHERS
+// Light switcher -  according to the hour
 const lightSwitch = document.getElementById('light-switch');
 lightSwitch.addEventListener('click', modeChanger);
 
 const colorMode = () => {
-   const currentHour = (new Date()).getHours();
-   console.log(currentHour);
-   if (currentHour < 6 || currentHour > 18) {
-      colorNightMode();
-      lightSwitch.classList.add('moon');
-      lightSwitch.classList.remove('sun');
-
-   } else {
-      colorDayMode();
-      lightSwitch.classList.add('sun');
-      lightSwitch.classList.remove('moon');
-
-   };
-}
+  const currentHour = new Date().getHours();
+  console.log(currentHour);
+  if (currentHour < 6 || currentHour > 18) {
+    colorNightMode();
+    lightSwitch.classList.add('moon');
+    lightSwitch.classList.remove('sun');
+  } else {
+    colorDayMode();
+    lightSwitch.classList.add('sun');
+    lightSwitch.classList.remove('moon');
+  }
+};
 colorMode();
 
-
-
-
-
-// Theme switcher - manual
+// Light switcher - manual
 function modeChanger() {
-if (lightSwitch.classList.contains('sun')) {
-   colorNightMode();
-   lightSwitch.classList.remove('sun');
-   lightSwitch.classList.add('moon');
-   console.log('night');
-
-} else if (lightSwitch.classList.contains('moon'))  {
-   colorDayMode();
-   lightSwitch.classList.add('sun');
-   lightSwitch.classList.remove('moon');
-   console.log('day');
-
+  if (lightSwitch.classList.contains('sun')) {
+    colorNightMode();
+    lightSwitch.classList.remove('sun');
+    lightSwitch.classList.add('moon');
+    console.log('night');
+  } else if (lightSwitch.classList.contains('moon')) {
+    colorDayMode();
+    lightSwitch.classList.add('sun');
+    lightSwitch.classList.remove('moon');
+    console.log('day');
+  }
 }
 
-}
-
+// //Background Switcher - Random
+// const backSwitch = document.getElementById('back-switch');
+// backSwitch.addEventListener('click', backChanger);
+//
+// function backChanger() {}
+//
+// // Image List Object
+// let backList = {};
